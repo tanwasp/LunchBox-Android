bb15f0e40b1022bb004e554ebd28056597c3226d
tanwasp <tanishwasp10@gmail.com>
2023-10-24 16:53:01 -0400

    added more javaDoc

--- intellij/prototype/src/model/Restaurant.java
+++ intellij/prototype/src/model/Restaurant.java
@@ -1,37 +1,111 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+        import java.util.ArrayList;
+        import java.util.Map;
+        import java.util.HashMap;
 
-
+/**
+ * Represents a restaurant with its details and reviews.
+ */
 public class Restaurant {
+
+    /**
+     * Unique identifier for the restaurant.
+     */
     public String restaurantId;
+
+    /**
+     * Name of the restaurant.
+     */
     public String name;
+
+    /**
+     * Average rating of the restaurant.
+     */
     public float rating;
+
+    /**
+     * Address of the restaurant.
+     */
     public String address;
+
+    /**
+     * City where the restaurant is located.
+     */
     public String city;
+
+    /**
+     * State where the restaurant is located.
+     */
     public String state;
+
+    /**
+     * Country where the restaurant is located.
+     */
     public String country;
+
+    /**
+     * Postal code of the restaurant.
+     */
     public String postalCode;
+
+    /**
+     * Latitude of the restaurant's location.
+     */
     public float lat;
+
+    /**
+     * Longitude of the restaurant's location.
+     */
     public float lon;
+
+    /**
+     * List of review IDs associated with the restaurant.
+     */
     public ArrayList<String> reviewList;
+
+    /**
+     * Distance of the restaurant from the user.
+     */
     public float distanceToUser;
+
+    /**
+     * Price range of the restaurant (represented by the number of dollar signs).
+     */
     public int priceRange;
 
-    public void computeRating(ReviewsLibrary revLib){
+    /**
+     * Computes the average rating of the restaurant based on its reviews.
+     *
+     * @param revLib The reviews library containing all reviews.
+     */
+    public void computeRating(ReviewsLibrary revLib) {
         if (this != null) {
             ArrayList<Review> reviews = revLib.getReviews(reviewList);
             float sum = 0;
-            for (Review rev: reviews){
+            for (Review rev : reviews) {
                 sum += rev.rating;
             }
             rating = sum / reviews.size();
         }
     }
 
-
+    /**
+     * Constructs a new Restaurant with the given parameters.
+     *
+     * @param restaurantId Unique identifier for the restaurant.
+     * @param name         Name of the restaurant.
+     * @param rating       Average rating of the restaurant.
+     * @param address      Address of the restaurant.
+     * @param city         City where the restaurant is located.
+     * @param state        State where the restaurant is located.
+     * @param country      Country where the restaurant is located.
+     * @param postalCode   Postal code of the restaurant.
+     * @param lat          Latitude of the restaurant's location.
+     * @param lon          Longitude of the restaurant's location.
+     * @param reviewList   List of review IDs associated with the restaurant.
+     * @param priceRange   Price range of the restaurant.
+     */
     public Restaurant(String restaurantId, String name, float rating, String address, String city, String state, String country, String postalCode, float lat, float lon, ArrayList<String> reviewList, int priceRange) {
         this.restaurantId = restaurantId;
         this.name = name;
@@ -47,8 +121,14 @@ public class Restaurant {
         this.priceRange = priceRange;
         distanceToUser = -1.0f;
     }
+
+    /**
+     * Returns a string representation of the restaurant.
+     *
+     * @return A formatted string containing restaurant details.
+     */
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder output = new StringBuilder();
         output.append("Name: ").append(name).append("\n");
         output.append("Rating: ").append(String.format("%.1f", rating)).append("\n");
@@ -70,38 +150,60 @@ public class Restaurant {
         return output.toString();
     }
 
+    /**
+     * Converts the price range of the restaurant to a string of dollar signs.
+     *
+     * @param priceRange Price range of the restaurant.
+     * @return A string of dollar signs representing the price range.
+     */
     public String getDollarSigns(int priceRange) {
         StringBuilder dollarSigns = new StringBuilder();
         for (int i = 0; i < priceRange; i++) {
             dollarSigns.append("$");
         }
         return dollarSigns.toString();
-
     }
 
-    public void setDistToUser(User u){
-        if (distanceToUser == -1.0f){
+    /**
+     * Sets the distance of the restaurant from the user.
+     *
+     * @param u The user for whom the distance is to be computed.
+     */
+    public void setDistToUser(User u) {
+        if (distanceToUser == -1.0f) {
             distanceToUser = haversine(u.lat, u.lon, this.lat, this.lon);
         }
     }
 
-    // help source: https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
-    private float haversine(float long1, float lat1, float long2, float lat2){
-        double earthRadius = 6371.0;
+        /**
+         * Computes the distance between two points using the Haversine formula.
+         *
+         * @param long1 Longitude of the first point.
+         * @param lat1  Latitude of the first point.
+         * @param long2 Longitude of the second point.
+         * @param lat2  Latitude of the second point.
+         * @return The distance between the two points in miles.
+         */
+        private float haversine(float long1, float lat1, float long2, float lat2){
 
-        // Convert latitude and longitude from degrees to radians
-        double lat1Rad = Math.toRadians(lat1);
-        double long1Rad = Math.toRadians(long1);
-        double lat2Rad = Math.toRadians(lat2);
-        double long2Rad = Math.toRadians(long2);
+            double earthRadius = 6371.0;
 
-        // Haversine formula
-        double dlong = long2Rad - long1Rad;
-        double dlat = lat2Rad - lat1Rad;
-        double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.pow(Math.sin(dlong / 2), 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = earthRadius * c;
+            // Convert latitude and longitude from degrees to radians
+            double lat1Rad = Math.toRadians(lat1);
+            double long1Rad = Math.toRadians(long1);
+            double lat2Rad = Math.toRadians(lat2);
+            double long2Rad = Math.toRadians(long2);
 
-        return (float) distance * 0.621371f;
+            // Haversine formula
+            double dlong = long2Rad - long1Rad;
+            double dlat = lat2Rad - lat1Rad;
+            double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.pow(Math.sin(dlong / 2), 2);
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            double distance = earthRadius * c;
+
+            // Convert distance from kilometers to miles
+            return (float) distance * 0.621371f;
+        }
     }
-}
+
+
