4d8f270f20571257b99a42baa8b82c22376cfb3e
CaroAK <CarolineAliciaKlein@icloud.com>
2023-11-06 23:26:22 -0500

    Add MainView

---
+++ astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/IMainView.java
@@ -0,0 +1,26 @@
+package edu.vassar.cmpu203.lunchbox.view;
+
+import android.view.View;
+import androidx.fragment.app.Fragment;
+
+/**
+ * An interface for the application screen template.
+ */
+public interface IMainView {
+
+    /**
+     * Retrieve the graphical widget (android view) at the root of the screen hierarchy
+     *
+     * @return the screen's root android view (widget)
+     */
+    View getRootView();
+
+    /**
+     * Replaces the contents of the screen's fragment container with the one passed in as an argument.
+     *
+     * @param fragment The fragment to be displayed.
+     * @param reversible true if this transaction should be reversible, false otherwise
+     * @param name the name this transaction can be referred by.
+     */
+    void displayFragment(Fragment fragment, boolean reversible, String name);
+}
---
+++ astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/MainView.java
@@ -0,0 +1,49 @@
+package edu.vassar.cmpu203.lunchbox.view;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import edu.vassar.cmpu203.lunchbox.databinding.MainBinding;
+
+
+public class MainView implements IMainView{
+
+    FragmentManager fmanager; // lets us perform fragment transactions
+    MainBinding binding; // gives us access to all the graphical components in res/layout/main.xml
+
+    /**
+     * Constructor method.
+     * @param activity The android activity the screen is associated with.
+     */
+    public MainView(FragmentActivity activity){
+        this.fmanager = activity.getSupportFragmentManager();
+        this.binding = MainBinding.inflate(activity.getLayoutInflater());
+    }
+
+    /**
+     * Retrieve the graphical widget (android view) at the root of the screen hierarchy/
+     * @return the screen's root android view (widget)
+     */
+    @Override
+    public View getRootView() {
+        return this.binding.getRoot();
+    }
+
+    /**
+     * Replaces the contents of the screen's fragment container with the one passed in as an argument.
+     *
+     * @param fragment The fragment to be displayed.
+     * @param addToStack true if this transaction should be reversible, false otherwise
+     * @param name the name this transaction can be referred by.
+     */
+    @Override
+    public void displayFragment(Fragment fragment, boolean addToStack, String name) {
+        FragmentTransaction ft = fmanager.beginTransaction();
+        ft.replace(this.binding.fragmentContainer.getId(), fragment);
+        if (addToStack)  ft.addToBackStack(name);
+        ft.commit();
+    }
+}
