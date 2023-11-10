7bcaad72f2b0f351c49dcdde84899eef0ddf0401
tanwasp <tanishwasp10@gmail.com>
2023-11-10 16:50:55 -0500

    adding clickable restaurants

--- astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/HomeFragment.java
+++ astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/HomeFragment.java
@@ -48,5 +48,3 @@ public class HomeFragment extends Fragment implements IHomeView{
     }
 
 }
-
-
--- astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/RestaurantFragment.java
+++ astudio/app/src/main/java/edu/vassar/cmpu203/lunchbox/view/RestaurantFragment.java
@@ -1,39 +1,39 @@
-//package edu.vassar.cmpu203.lunchbox.view;
-//
-//import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.fragment.app.Fragment;
-//
-//import edu.vassar.cmpu203.lunchbox.databinding.FragmentRestaurantBinding;
-//
-//public class RestaurantFragment extends Fragment implements IRestaurantView{
-//    private final Listener listener;
-//    private FragmentRestaurantBinding binding;
-//
-//    public RestaurantFragment(Listener listener){
-//        this.listener = listener;
-//    }
-//
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-//        this.binding = FragmentRestaurantBinding.inflate(inflater);
-//        return this.binding.getRoot();
-//    }
-//
-//    @Override
-//    public void OnViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
-//        super.onViewCreated(view, savedInstanceState);
-//        this.binding.btnNavigateToPostReview.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                listener.onNavigateToPostReview();
-//            }
-//        });
-//    }
-//
-//}
+package edu.vassar.cmpu203.lunchbox.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.vassar.cmpu203.lunchbox.databinding.FragmentRestaurantBinding;
+
+public class RestaurantFragment extends Fragment implements IRestaurantView{
+    private final Listener listener;
+    private FragmentRestaurantBinding binding;
+
+    public RestaurantFragment(Listener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        this.binding = FragmentRestaurantBinding.inflate(inflater);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void OnViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        this.binding.btnNavigateToPostReview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                listener.onNavigateToPostReview();
+            }
+        });
+    }
+
+}
