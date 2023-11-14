220c2edc64f0734706c4a7d9070e81881eeaf4a9
tanwasp <tanishwasp10@gmail.com>
2023-11-14 13:00:38 -0500

    adding unit tests

--- astudio/app/build.gradle.kts
+++ astudio/app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
---
+++ astudio/app/src/test/java/edu/vassar/cmpu203/lunchbox/model/ReviewsLibraryTest.java
@@ -0,0 +1,16 @@
+package edu.vassar.cmpu203.lunchbox.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReviewsLibraryTest {
+
+    @Test
+    void addReview() {
+    }
+
+    @Test
+    void getReviews() {
+    }
+}
