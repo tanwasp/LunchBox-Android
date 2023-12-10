3b52e038eaccce7fc2ef84012771d8e9591f4f98
tanwasp <tanishwasp10@gmail.com>
2023-12-10 17:41:30 -0500

    added checkuser profile test

--- astudio/app/src/androidTest/java/edu/vassar/cmpu203/lunchbox/CheckProfileTest.java
+++ astudio/app/src/androidTest/java/edu/vassar/cmpu203/lunchbox/CheckProfileTest.java
@@ -1,5 +1,6 @@
 package edu.vassar.cmpu203.lunchbox;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -8,19 +9,32 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.google.common.base.Predicates.instanceOf;
+
+import static org.hamcrest.Matchers.is;
+
 import android.os.SystemClock;
+import android.view.MenuItem;
+
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.GrantPermissionRule;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import edu.vassar.cmpu203.lunchbox.controller.MainActivity;
+import edu.vassar.cmpu203.lunchbox.model.FirestoreSingleton;
 
 @RunWith(AndroidJUnit4.class)
 public class CheckProfileTest {
@@ -34,16 +48,20 @@ public class CheckProfileTest {
      * Tests ability to navigate to profile page and view profile info
      */
     @Test
-    public void testCheckProfile(){
+    public void testCheckProfile() throws UiObjectNotFoundException {
         // Log in to account
         onView(withId(R.id.btnLogin)).perform(click());
         onView(withId(R.id.email)).perform(typeText("johndoe@gmail.com"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.password)).perform(typeText("abc123"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
-        // test navigation
-        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_profile));
-        SystemClock.sleep(2000);
-
+        // Open the navigation drawer
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        SystemClock.sleep(1000); // Wait for drawer to open
+        activityRule.getScenario().onActivity(activity -> {
+            activity.getUserReviewsNavToProfile(); // Call the method here
+        });
+        onView(withText("john80")).check(matches(isDisplayed()));
     }
+
 }
